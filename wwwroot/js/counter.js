654a3ffdd4142e070d5f9b1f89e68940812543b5
DreamEnderKing <www20040718@163.com>
2020-08-19 13:10:30 +0800

    Update

--- wwwroot/js/counter.js
+++ wwwroot/js/counter.js
@@ -1,6 +1,7 @@
 window.drawPoint = (x, y) => {
     var x = document.getElementById("canvas");
     var can = x.getContext("2d");
-    can.Scale(0.4， 0.4);
-    can.FillRect(x, y, x + 2, y + 2);
+    can.scale(0.4, 0.4);
+    can.fillRect(x, y, x + 2, y + 2);
+    return 0;
 }
