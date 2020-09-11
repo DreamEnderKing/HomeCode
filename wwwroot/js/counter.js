af606453b73ea672700f47a48b4e0a0bc03dea90
test <qq@com>
2020-09-11 15:33:57 +0800

    Update

--- wwwroot/js/counter.js
+++ wwwroot/js/counter.js
@@ -10,7 +10,8 @@ function canvasLoad(){
     can = document.getElementById("canvas").getContext("2d");
     if(!isScaled) {
         can.arc(0, 0, 200, 0, 2 * Math.PI);
-        can.scale(0.4, 0.4);
+        can.rect(0, 0, 200, 200);
+        //can.scale(0.4, 0.4);
         isScaled = true;
     }
     return can;
