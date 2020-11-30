127e2ce5afa2925763ac7b9ecfc12b6bc027edfe
DreamEnderKing <58102615+DreamEnderKing@users.noreply.github.com>
2020-11-30 19:26:59 +0800

    Update counter.js

--- wwwroot/js/counter.js
+++ wwwroot/js/counter.js
@@ -1,5 +1,6 @@
 window.drawPoint = (x, y, r) => {
-    can = canvasLoad();
+    can = document.getElementById("canvas").getContext("2d");
+    can.fillStyle = "#FF0000";
     //window.alert("Me");
     can.fillRect(x, y, r, r);
     can.fill();
@@ -9,18 +10,9 @@ window.drawPoint = (x, y, r) => {
 window.isScaled = false;
 function canvasLoad(){
     can = document.getElementById("canvas").getContext("2d");
-    if(!isScaled) {
-        can.fillStyle = "#00FF00";
-        can.arc(0, 0, 200, 0, 2 * Math.PI);
-        can.fill();
-        can.strokeStyle = "#000000";
-        can.strokeRect(0, 0, 200, 200);
-        //can.scale(0.4, 0.4);
-        
-        isScaled = true;
-    }
-    else {
-        can.fillStyle = "#FF0000";
-    }
-    return can;
+    can.fillStyle = "#00FF00";
+    can.arc(200, 200, 200, 0, 2 * Math.PI);
+    can.fill();
+    can.strokeStyle = "#000000";
+    can.strokeRect(0, 0, 200, 200);
 }
