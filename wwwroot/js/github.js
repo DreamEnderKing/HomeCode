66c91b402db438f13bd0c414323dcc872b219b4b
DreamEnderKing <58102615+DreamEnderKing@users.noreply.github.com>
2021-02-02 22:12:31 +0800

    fix a probable problem.

--- wwwroot/js/github.js
+++ wwwroot/js/github.js
@@ -1,4 +1,4 @@
-    <script type="text/javascript">
+
         (function(l) {
             if (l.search) {
                 var q = {};
@@ -15,4 +15,3 @@
                 }
             }
         }(window.location))
-    </script>
