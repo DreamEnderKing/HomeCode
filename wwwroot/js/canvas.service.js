f69cc8447ad6f459ed95ee20a9e2324c00ea026f
DreamEnderKing <58102615+DreamEnderKing@users.noreply.github.com>
2021-02-05 20:38:16 +0800

    Update canvas.service.js

--- wwwroot/js/canvas.service.js
+++ wwwroot/js/canvas.service.js
@@ -16,3 +16,13 @@ function SetShadow(key, color, blur, offsetX, offsetY)
    can.shadowOffsetX = offsetX;
    can.shadowOffsetY = offsetY;
 }
+function SetLineCap(key, cap)
+{
+   var can = #(key).getContext("2d");
+   can.lineCap = cap;
+}
+function SetLineJoin(key, join)
+{
+   var can = #(key).getContext("2d");
+   can.lineJoin = join;
+}
