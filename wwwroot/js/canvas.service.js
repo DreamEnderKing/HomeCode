131572b1c1dfa90f5e66021a4b312b6c07c1c51a
DreamEnderKing <58102615+DreamEnderKing@users.noreply.github.com>
2021-02-05 21:17:41 +0800

    Update canvas.service.js

--- wwwroot/js/canvas.service.js
+++ wwwroot/js/canvas.service.js
@@ -1,20 +1,23 @@
-function SetFillStyle(key, style)
+function SetFillStyle(key, style, alpha)
 {
    var can = $(key).getContext("2d");
    can.fillStyle = style;
+   can.globalAlpha = alpha;
 }
-function SetStrokeStyle(key, style)
+function SetStrokeStyle(key, style,alpha)
 {
    var can = $(key).getContext("2d");
    can.strokeStyle = style;
+   can.globalAlpha = alpha;
 }
-function SetShadow(key, color, blur, offsetX, offsetY)
+function SetShadow(key, color, alpha, blur, offsetX, offsetY)
 {
    var can = $(key).getContext("2d");
    can.shadowColor = color;
    can.shadowBlur = blur;
    can.shadowOffsetX = offsetX;
    can.shadowOffsetY = offsetY;
+   can.globalAlpha = alpha;
 }
 function SetLineCap(key, cap)
 {
@@ -25,6 +28,16 @@ function SetLineJoin(key, join)
 {
    var can = $(key).getContext("2d");
    can.lineJoin = join;
+}
+function SetLineWidth(key, width)
+{
+   var can = $(key).getContext("2d");
+   can.lineWidth = width;
+}
+function SetLineMiter(key, width)
+{
+   var can = $(key).getContext("2d");
+   can.miterLimit = width;
 }
 
 function FillRect(key, left, top, width, height)
