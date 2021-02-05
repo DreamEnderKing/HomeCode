b0a5a1c3b8407e601dc21d7ada29a5ba17252dec
DreamEnderKing <www20040718@163.com>
2021-02-05 15:09:21 +0800

    Update the canvas service.

---
+++ Service/Canvas.Service.cs
@@ -0,0 +1,14 @@
+using System.Text;
+using System;
+
+
+namespace BS
+{
+    public class Canvas2D
+    {
+        public String Hello() {
+            
+            return "Hello!";
+        }
+    }
+}
