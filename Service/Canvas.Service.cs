ef09cf06d2585332922eb6a88dcb10c07c6b7402
DreamEnderKing <www20040718@163.com>
2021-02-06 14:14:19 +0800

    Update

--- Service/Canvas.Service.cs
+++ Service/Canvas.Service.cs
@@ -1,11 +1,23 @@
 using System.Text;
 using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.JSInterop;
 
 
 namespace BS
 {
     public class Canvas2D
     {
+        private IJSRuntime JS;
+        private String Key;
+        private bool isInited = false;
+        public void Init(IJSRuntime JSRuntime, String key = "#canvas")
+        {
+            JS = JSRuntime;
+            Key = key;
+            isInited = true;
+        }
         public String Hello() {
             
             return "Hello!";
